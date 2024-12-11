d6d05848a041a75816dc3f4f706d6f489d295b52
jacu <jacu@ailleron.com>
2024-12-12 00:02:00 +0100

    init

---
+++ BelayrChat/Hubs/ChatHub.cs
@@ -0,0 +1,54 @@
+﻿using BelayrChat.Models;
+using Microsoft.AspNetCore.SignalR;
+using Hub = Microsoft.AspNetCore.SignalR.Hub;
+
+namespace BelayrChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly IDictionary<string, UserRoomConnection> _connection;
+
+        public ChatHub(IDictionary<string, UserRoomConnection> connection)
+        {
+            _connection = connection;
+        }
+
+        public async Task JoinRoom(UserRoomConnection connection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, connection.Room);
+            _connection[Context.ConnectionId] = connection;
+            await Clients.Group(connection.Room)
+                .SendAsync("ReceiveMessage", "Belayr Bot", $"{connection.User} dołączył do pokoju", DateTime.Now);
+            await SendConnectedUser(connection.Room);
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if (_connection.TryGetValue(Context.ConnectionId, out UserRoomConnection userRoomConnection))
+            {
+                await Clients.Group(userRoomConnection.Room)
+                    .SendAsync("ReceiveMessage", userRoomConnection.User, message, DateTime.Now);
+            }
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            if (_connection.TryGetValue(Context.ConnectionId, out UserRoomConnection? userRoomConnection) == false)
+            {
+                return base.OnDisconnectedAsync(exception);
+            }
+
+            _connection.Remove(Context.ConnectionId);
+            Clients.Group(userRoomConnection.Room).SendAsync("ReceiveMessage", "Belayr Bot",
+                $"{userRoomConnection.User} opuścił pokój", DateTime.Now);
+            SendConnectedUser(userRoomConnection.Room);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public Task SendConnectedUser(string room)
+        {
+            var users = _connection.Values.Where(x => x.Room == room).Select(x => x.User);
+            return Clients.Group(room).SendAsync("ConnectedUser", users);
+        }
+    }
+}
